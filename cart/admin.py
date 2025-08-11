f2f9807add981ce306ddc000fd006ceb3c31e25d
m1gahara <zartt6484@gmail.com>
2025-08-11 18:12:04 +0300

    добавили cart и функционал

---
+++ cart/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Cart, CartItem
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+    readonly_fields = ("total_price_display",) #Исправлено
+
+    def total_price_display(self, obj): #Добавлено
+        return obj.total_price
+    total_price_display.short_description = 'Total Price'
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("session_key", "total_items", "subtotal", "created_at",
+                    "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("session_key", )
+    inlines = [CartItemInline]
+    readonly_fields = ("total_items", "subtotal")
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ("cart", "product", "product_size",
+                    "quantity", "total_price_display", "added_at") #Исправлено
+    list_filter = ("added_at",) #Добавлена запятая
+    search_fields = ("product__name", "cart__session_key")
+    readonly_fields = ("total_price_display",) #Исправлено
+
+    def total_price_display(self, obj): #Добавлено
+        return obj.total_price
+    total_price_display.short_description = 'Total Price'
